4f7c1af384ac501b1c93bc7f764873b2cc3ee692
ibrahim <ibrahimayoob@gmail.com>
2014-06-08 19:55:32 +0800

    minor code cleanup

--- lib/ooyala_query_video.php
+++ lib/ooyala_query_video.php
@@ -1,13 +1,9 @@
 <?php
 
   require_once 'sdk/OoyalaApi.php';
-
-  //header('Content-type: application/json');
+  
   header('Content-type: text/plain');
 
-  //$op_result = 0;
-  //$op_msg;
-  //$op_videoname;
   $results;  
   $response = array();
   
@@ -21,9 +17,7 @@
                         "expires" => time());
     $results = $api->get("assets", $parameters);    
   } catch (Exception $e) {
-    //$op_result=1;
     $response[0]['op_result'] = 1;
-    //$op_msg = $e->getMessage();
     $response[0]['op_msg'] = $e->getMessage(); 
   }
 
@@ -31,36 +25,18 @@
   $assets = $results->items;  
   // results might be empty even if there was no exception raised
   if(empty($assets)==true && $response[0]['op_result']==0) {
-    //$op_result = 1;
     $response[0]['op_result'] = 1;
-    //$op_msg = "Nothing returned from Backlot API";
     $response[0]['op_msg'] = "Nothing returned from Backlot API";  
   }  
   
   if($response[0]['op_result']==0) {
-    foreach($assets as $asset) {       
-        //$asset_array = get_object_vars($asset);
-        //$asset_array['op_result'] = $op_result;
-        //$asset_array['op_msg'] = $op_msg;
-        $response[0]['op_videoname'] = $asset->name;        
-        
-        //echo (json_encode(array_values($asset_array)));                        
-        //echo (json_encode($response));
+    foreach($assets as $asset) {
+        $response[0]['op_videoname'] = $asset->name;
         print($response[0]['op_videoname']); 
     }  
   }
-  else {        
-        /*
-        $asset['op_result'] = $op_result;
-        $asset['op_msg'] = $op_msg;        
-        echo json_encode($asset,JSON_PRETTY_PRINT);
-        */
-        //echo (json_encode($response));
+  else {
         print("");
-  } 
-    
-  //var_dump($response);
-  //echo (json_encode($results));
-
+  }
 ?>
 
--- lib/ooyala_set_title.php
+++ lib/ooyala_set_title.php
@@ -16,17 +16,13 @@
   $api = new OoyalaApi("ZzcTMyOuDOdGDu3v1-rBTsPtL2wW.LN1Ce", "t8Vq02Y6XuTHDIo68GXB0TfR7NlPo9XBKB0PfJ7k");
   
   $parameters = array("name" => $title);
-  $results = $api->patch("assets/5meW03bjoqTol7A177tuKqZtvw7Sr5EB", $parameters);  
-  //$assets = $results->items;
+  $results = $api->patch("assets/5meW03bjoqTol7A177tuKqZtvw7Sr5EB", $parameters);
   $asset = get_object_vars($results); 
   
   
-  echo "Dump video details: \n\n";
-  //  foreach($assets as $asset) {
-        //print_r("$results \n\n");                
+  echo "Dump video details: \n\n";                
         print_r("$asset \n\n");        
-        var_dump($results);
-  //}  
+        var_dump($results);  
 
   echo (json_encode($results));
 
